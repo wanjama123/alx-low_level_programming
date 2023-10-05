1ab870f7b389aa45c6610b60bb0e843988bdd977
Ian Wanjama <ianwanjama78@gmail.com>
2023-10-05 04:43:15 -0700

    Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The program that prints out text of puts function
+ * Return:0
+ */
+int main(void)
+{
+puts("\"programming is like biulding a multilingual puzzle");
+return (0);
+}
