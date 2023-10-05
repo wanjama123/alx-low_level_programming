a4c1f4e14a5323ee2c30c6839a2ab5ef01194ee0
Ian Wanjama <ianwanjama78@gmail.com>
2023-10-05 08:05:12 -0700

    Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main - The program that prints function error
- *Return:(0)
+ *main - The program that prints a line to the standard error
+ *Return: 0
  */
 int main(void)
 {
-int fputs( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
