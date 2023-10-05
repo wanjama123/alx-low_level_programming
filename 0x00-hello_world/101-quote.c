029bd6834c94f46932d43e888c634900829c2d0b
Ian Wanjama <ianwanjama78@gmail.com>
2023-10-05 07:30:32 -0700

    Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,6 @@
  *Return:(0)
  */ int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fputs( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
