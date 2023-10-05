23046035b3c61b980de8e0b1cc6d038d01f17a80
Ian Wanjama <ianwanjama78@gmail.com>
2023-10-05 06:18:21 -0700

    Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- The program that prints a line to the standard error
+ * Return:0
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+return (1);
+}
