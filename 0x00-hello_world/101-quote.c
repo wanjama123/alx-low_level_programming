2f077972d33f1681fff6ed13f26908a3d5452a7f
Ian Wanjama <ianwanjama78@gmail.com>
2023-10-05 07:38:27 -0700

    Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main - Entry point
  *Return:(0)
- */ int main(void)
+ */
+int main(void)
 {
 fputs( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
