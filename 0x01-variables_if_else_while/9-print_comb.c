a0ff35951a396f213375f8f10d67f4c7c7033719
Ian Wanjama <ianwanjama78@gmail.com>
2023-10-06 06:37:17 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the all possible combinstions of single digits numbers
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 48; n < 58; i++)
+{
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+return (0);
+}
