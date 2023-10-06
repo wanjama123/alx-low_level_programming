aa9597a651956fabb84b0e2c07702fcb10f30df3
Ian Wanjama <ianwanjama78@gmail.com>
2023-10-06 05:58:09 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 0 to 10
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
