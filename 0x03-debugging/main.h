9b5e39a634a9b92fc55eb4480228196cc6fdf690
Ian Wanjama <ianwanjama78@gmail.com>
2023-10-13 04:24:38 -0700

    C - Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-int convert _day(int month, int day);
+int convert_day(int month, int day);
 void printf_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
